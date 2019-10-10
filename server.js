2b36fa978d2fc4a7037b06d110975f46314dd7ba
Tobias Sommer <togo210@googlemail.com>
2019-10-10 22:33:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ var uuid = require('uuid');
 var bluebird = require('bluebird');
 var connectionString = ('postgres://mrikelctdoichh:c7e5955d89d75e3af7d8ab6e8037a9b1a37370477207b092175de425087faed1@ec2-176-34-183-20.eu-west-1.compute.amazonaws.com:5432/d2siu1ipm9dhqj');
 
-// make debuggin' a bit better
-bluebird.longStackTraces();
-
 var pg = require('pg');
 console.log('Thats me');
 
