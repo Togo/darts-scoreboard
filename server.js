eeb53f77f6e798b55556a1d9bde0bc2fec3b1349
Tobias Sommer <togo210@googlemail.com>
2019-10-10 22:07:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var pgp = require('promise-pg')(/* options */)
+var pgp = require('pg')(/* options */)
 var db = pgp('postgres://mrikelctdoichh:c7e5955d89d75e3af7d8ab6e8037a9b1a37370477207b092175de425087faed1@ec2-176-34-183-20.eu-west-1.compute.amazonaws.com:5432/d2siu1ipm9dhqj')
 
 db.one('SELECT $1 AS value', 123)
