7e777244cc4792e19a5730652aa94ec3904d4e1b
Tobias Sommer <togo210@googlemail.com>
2019-10-10 22:46:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var uuid = require('uuid');
 var bluebird = require('bluebird');
 var connectionString = ('postgres://mrikelctdoichh:c7e5955d89d75e3af7d8ab6e8037a9b1a37370477207b092175de425087faed1@ec2-176-34-183-20.eu-west-1.compute.amazonaws.com:5432/d2siu1ipm9dhqj');
 
-var pg = require('pg');
+const pg = require('pg');
 console.log('Thats me');
 
 // monkey patch pg.Client.query to console.log for testing purposes only
